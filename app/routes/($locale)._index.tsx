f9b436975545b739fbad2a77822e0cd6d74aa5ff
ekamcn <ekamjit@capitalnumbers.com>
2025-08-22 08:22:51 +0000

    automot done

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -27,10 +27,10 @@ const sections = [
             <p>
               Si vous ne trouvez pas la réponse à votre question, merci de nous envoyer un e-mail à{' '}
               <a
-                href="mailto:contact@refletjardin.com"
-                className="hover:text-blue-300 transition-colors !text-[var(--color-1)] underline underline-offset-4"
-              >
-                contact@refletjardin.com
+                   href={`mailto:${import.meta.env.VITE_CUSTOMER_SUPPORT_EMAIL}`}
+                   className=" hover:text-blue-300 transition-colors !text-[var(--color-footer)] underline underline-offset-4 text-wrap"
+                 >
+                   {import.meta.env.VITE_CUSTOMER_SUPPORT_EMAIL}
               </a>
             </p>
           </div>
@@ -135,8 +135,8 @@ export default function Homepage() {
         title="Cosy Critters3 "
         imageUrl={import.meta.env.VITE_BANNER}
         mobileImageUrl={import.meta.env.VITE_MOBILE_BANNER}
-        subtitle="Bienvenue chez Esprit Auto Moto, la boutique en ligne pensée par des passionnés, pour des passionnés."
-        description="Que vous rouliez en voiture ou en deux-roues, que vous soyez amateur de tuning, adepte de sensations fortes ou simplement soucieux de bien entretenir votre véhicule, Esprit Auto Moto est là pour vous équiper avec style, efficacité et performance."
+        subtitle={`Bienvenue chez ${import.meta.env.VITE_STORE_TITLE} , la boutique en ligne pensée par des passionnés, pour des passionnés.`}
+        description={`Que vous rouliez en voiture ou en deux-roues, que vous soyez amateur de tuning, adepte de sensations fortes ou simplement soucieux de bien entretenir votre véhicule, ${import.meta.env.VITE_STORE_TITLE} est là pour vous équiper avec style, efficacité et performance.`}
         buttonText="Shop Now"
         buttonUrl=""
       />
